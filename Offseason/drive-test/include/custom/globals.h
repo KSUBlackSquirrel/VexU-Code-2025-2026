e2fc6f9847f205cf41b325daab3a32a464c08a8c
philiptarmelie <philiptarmelie@gmail.com>
2025-09-10 20:19:38 -0400

    update

--- Offseason/drive-test/include/custom/globals.h
+++ Offseason/drive-test/include/custom/globals.h
@@ -1,30 +1,9 @@
 #ifndef _GLOBALS_H_
 #define _GLOBALS_H_
 
-namespace MotorTools {
-inline int percentToVelocity(int percent, pros::MotorGears color) {
-    int maxRpm = 0;
-    switch (color) {
-        case pros::MotorGearset::red:   maxRpm = 100; break;
-        case pros::MotorGearset::green: maxRpm = 200; break;
-        case pros::MotorGearset::blue:  maxRpm = 600; break;
-        default: maxRpm = 0; break;
-    }
-    return static_cast<int>(percent * maxRpm / 100.0);
+namespace gobalDrive {
+    joystickDeadband
+    expoCurve
 }
-} // namespace motor tools 
-
-namespace GlobalControl {
-const static pros::controller_analog_e_t leftStickY = pros::E_CONTROLLER_ANALOG_LEFT_Y;
-const static pros::controller_analog_e_t rightStickY = pros::E_CONTROLLER_ANALOG_RIGHT_Y;
-const static int joystickDeadband = 1;
-const static double expoCurve = 1.0;
-} // namespace control 
-
-
-namespace GlobalMotor {
-const static std::int8_t motorId = 1;
-const static pros::MotorGears motorColor = pros::MotorGearset::red;
-} // namespace motor 
 
 #endif
--- Offseason/drive-test/include/main.h
+++ Offseason/drive-test/include/main.h
@@ -40,7 +40,11 @@
  * You should add more #includes here
  */
 //#include "okapi/api.hpp"
-
+#include "lemlib/api.hpp"
+#include "custom/globals.h"
+#include "custom/subsystem/subsystemBase.h"
+#include "custom/command/commandBase.h"
+#include "custom/command/instantCommand.h"
 /**
  * If you find doing pros::Motor() to be tedious and you'd prefer just to do
  * Motor, you can use the namespace with the following commented out line.
